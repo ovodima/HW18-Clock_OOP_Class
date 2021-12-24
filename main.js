bd926092897d97f842cc9bbe735bb38d469bdc62
Dima <dima.tsymbal12345@gmail.com>
2021-12-24 14:24:12 +0200

    home work Done

--- main.js
+++ main.js
@@ -3,88 +3,96 @@ const interval = 250;
 const time1 = document.querySelector(".time-1");
 const time2 = document.querySelector(".time-2");
 const time3 = document.querySelector(".time-3");
-// let check = time.classList.contains('full')
+// let check = main.classList.contains('full')
+let bool = true;
 
 class Clock {
-    constructor() {
-        this.clock = new Date()
-        this.formats = []
-    }
+  constructor() {
+    this.clock = new Date();
+    this.formats = [];
+  }
 
-    getTime() {
-        this.formats.push(this.clock.toUTCString())
-    }
+  getTime() {
+    this.formats.push(this.clock.toUTCString());
+  }
 
-    getFormats() {
-        return this.formats
-    }
+  getFormats() {
+    return this.formats;
+  }
 
-    render(elem) {
-        this.getTime()
-        let formats = this.getFormats()
-        
-        formats.filter(format => {
-            if(format.includes(this.getTime())) {
-                elem.innerText = format
-            }
-        })
+  render(elem) {
+    this.getTime();
+    let formats = this.getFormats();
 
-        
-    }
-        
-    }
-
+    formats.filter((format) => {
+      if (format.includes(this.getTime())) {
+        elem.innerText = format;
+      }
+    });
+  }
+}
 
 class Full extends Clock {
-    constructor() {
-        super(new Date())
-        this.hour = this.clock.getHours()
-        this.minutes = this.clock.getMinutes()
-        this.second = this.clock.getSeconds()
-    }
+  constructor() {
+    super(new Date());
+    this.hour = this.clock.getHours();
+    this.minutes = this.clock.getMinutes();
+  }
 
-    getTime() {
-        let localString = new Date().toLocaleTimeString()
-        this.formats.push(localString)
-        return localString
+  getTime() {
+    let fullTime;
+    if (!bool) {
+      fullTime = this.clock.toLocaleTimeString();
+    } else if (bool) {
+      fullTime = `${this.hour}:${this.minutes}`;
+    } else {
+      return;
     }
+    this.formats.push(fullTime);
+    return fullTime;
+  }
 }
 
 class Short extends Clock {
-    constructor() {
-        super(new Date())
-        this.hour = this.clock.getHours()
-        this.minutes = this.clock.getMinutes()
+  constructor() {
+    super(new Date());
+    this.hour = this.clock.getHours();
+    this.minutes = this.clock.getMinutes();
+  }
+
+  getTime() {
+    let shortFormat;
+
+    if (bool) {
+      shortFormat = this.clock.toLocaleTimeString();
+    } else if (!bool) {
+      if (this.hour < 10) this.hour = "0" + this.hour;
+      if (this.minutes < 10) this.minutes = "0" + this.minutes;
+      shortFormat = `${this.hour}:${this.minutes}`;
+    } else {
+      return;
     }
 
-    getTime() {
-        if (this.hour < 10) this.hour = "0" + this.hour;
-        if (this.minutes < 10) this.minutes = "0" + this.minutes;
-        
-        let shortFormat = `${this.hour}:${this.minutes}`
-
-        this.formats.push(shortFormat)
-        return shortFormat
-    }
-
+    this.formats.push(shortFormat);
+    return shortFormat;
+  }
 }
 
-main.addEventListener('click', (e) => {
-    let target = e.target
-    target.classList.toggle('full')
-    console.log(e.target.classList)
-})
+main.addEventListener("click", () => {
+
+  if (bool) {
+    bool = !bool;
+  } else {
+    bool = true;
+  }
+});
 
 
 
 setInterval(() => {
-// let clock = new Clock()
-let short = new Short()
-let full = new Full()
+  let short = new Short();
+  let full = new Full();
 
-//  clock.render(time1)
- short.render(time2)
- full.render(time3)
+  short.render(time2);
+  full.render(time3);
 }, interval);
-
-
